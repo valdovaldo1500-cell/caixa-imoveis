450032e7ef07c4c8eca8c7387701efcf782fd752
Fortress Lite Developer <dev@fortress-lite.local>
2026-04-01 20:46:43 +0100

    checkpoint: Edit page.tsx

--- src/app/imoveis/page.tsx
+++ src/app/imoveis/page.tsx
@@ -2418,9 +2418,9 @@ function ImoveisPageInner() {
           />
         </div>
 
-        <Card className="bg-zinc-900 border-zinc-800 overflow-auto max-h-[calc(100vh-280px)]">
-          <CardContent className="p-0 pb-4">
-            <Table>
+        <Card className="bg-zinc-900 border-zinc-800">
+          <CardContent className="p-0 pb-4 overflow-auto max-h-[calc(100vh-280px)]" style={{ overflow: "auto", clipPath: "none" }}>
+            <Table className="overflow-visible">
               <TableHeader>
                 <TableRow className="border-zinc-800 hover:bg-zinc-800/50">
                   {visibleColumns.map((colId) => renderHeader(colId))}
